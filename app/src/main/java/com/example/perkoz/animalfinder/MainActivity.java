81a6f18f66a5d05e7c333084f369e4e808755e94
pawel2973 <37537809+pawel2973@users.noreply.github.com>
2018-12-15 21:43:53 +0100

    Animal Finder

---
+++ app/src/main/java/com/example/perkoz/animalfinder/MainActivity.java
@@ -0,0 +1,208 @@
+package com.example.perkoz.animalfinder;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.PorterDuff;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.cloud.FirebaseVisionCloudDetectorOptions;
+import com.google.firebase.ml.vision.cloud.label.FirebaseVisionCloudLabel;
+import com.google.firebase.ml.vision.cloud.label.FirebaseVisionCloudLabelDetector;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private View layout; // Background
+    private Switch nightSwitch; // Switch Background
+    private ImageView imageDisplay; // Displayed Image
+    private TextView textPrediction; // Predicted names
+    private ImageButton cameraButton, galleryButton, exitButton, infoButton; // APP Buttons
+
+
+    private final int REQUEST_IMAGE_CAPTURE = 1, REQUEST_IMAGE_GALLERY = 2; // REQUESTS
+    private FirebaseVisionImage imageFirebase; // Firebase Vision Image
+    private String animalName;  // Predicted animal name
+    public static Bitmap bitmapImage; // User image
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Default content
+        animalName = "hunter";
+        bitmapImage = BitmapFactory.decodeResource(getResources(), R.drawable.deerhunter);
+        bitmapImage = resizeImage(bitmapImage);
+
+        // Layout
+        layout = (View) findViewById(R.id.background);
+        nightSwitch = (Switch) findViewById(R.id.switchNight);
+        nightSwitch.setOnClickListener(this);
+
+        // Image View
+        imageDisplay = (ImageView) findViewById(R.id.imageView);
+        imageDisplay.setImageBitmap(bitmapImage);
+
+        // Image Buttons
+        cameraButton = (ImageButton) findViewById(R.id.imageButtonCamera);
+        cameraButton.setOnClickListener(this);
+        galleryButton = (ImageButton) findViewById(R.id.imageButtonGallery);
+        galleryButton.setOnClickListener(this);
+        exitButton = (ImageButton) findViewById(R.id.imageButtonExit);
+        exitButton.setOnClickListener(this);
+        infoButton = (ImageButton) findViewById(R.id.imageButtonInfo);
+        infoButton.setOnClickListener(this);
+
+        // Text Fields
+        textPrediction = (TextView) findViewById(R.id.textViewPrediction);
+    }
+
+    // -------------------- onClick Events --------------------
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.imageButtonCamera:
+                // Camera Support
+                Intent iCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (iCamera.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(iCamera, REQUEST_IMAGE_CAPTURE);
+                }
+                break;
+            case R.id.imageButtonGallery:
+                // Gallery Support
+                Intent iGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                iGallery.setType("image/*");
+                startActivityForResult(iGallery, REQUEST_IMAGE_GALLERY);
+                break;
+            case R.id.imageButtonExit:
+                finish();
+                System.exit(0);
+            case R.id.switchNight:
+                // Switch Support
+                if (nightSwitch.isChecked()) {
+                    layout.setBackground(getDrawable(R.drawable.starfield_background));
+                } else {
+                    layout.setBackgroundResource(R.color.colorPrimary);
+                }
+                break;
+            case R.id.imageButtonInfo:
+                // Wikipedia Support
+                // Change Image bitmap to Byte Array (Better format to send between activities
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] byteArray = stream.toByteArray();
+
+                Intent intent = new Intent(MainActivity.this, Wikipedia.class);
+                intent.putExtra("animalname", animalName); // add animal name to new activity
+                startActivity(intent);
+                break;
+        }
+    }
+
+    // -------------------- onActivity Result --------------------
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_IMAGE_CAPTURE) {
+                //PHOTO FROM CAMERA
+                bitmapImage = (Bitmap) data.getExtras().get("data");
+                imageDisplay.setImageBitmap(bitmapImage);
+                bitmapImage = resizeImage(bitmapImage);
+                imageFirebase = FirebaseVisionImage.fromBitmap(bitmapImage);
+                textPrediction.setText("Loading...");
+                labelImagesCloud(imageFirebase);
+            } else if (requestCode == REQUEST_IMAGE_GALLERY) {
+                //PHOTO FROM GALLERY
+                Uri uri = data.getData();
+                bitmapImage = null;
+                try {
+                    bitmapImage = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                    imageDisplay.setImageBitmap(bitmapImage);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                imageDisplay.setImageBitmap(bitmapImage);
+                bitmapImage = resizeImage(bitmapImage);
+                imageFirebase = FirebaseVisionImage.fromBitmap(bitmapImage);
+                textPrediction.setText("Loading...");
+                labelImagesCloud(imageFirebase);
+            }
+        }
+    }
+
+    private Bitmap resizeImage(Bitmap image) {
+        float aspectRatio = image.getWidth() /
+                (float) image.getHeight();
+        int width = 480;
+        int height = Math.round(width / aspectRatio);
+
+        image = Bitmap.createScaledBitmap(
+                image, width, height, false);
+
+        return image;
+    }
+
+    private void labelImagesCloud(FirebaseVisionImage image) {
+        // [START set_detector_options_cloud]
+        FirebaseVisionCloudDetectorOptions options = new FirebaseVisionCloudDetectorOptions.Builder()
+                .setModelType(FirebaseVisionCloudDetectorOptions.LATEST_MODEL)
+                .setMaxResults(5)
+                .build();
+        // [END set_detector_options_cloud]
+
+        // [START get_detector_cloud]
+        FirebaseVisionCloudLabelDetector detector = FirebaseVision.getInstance()
+                .getVisionCloudLabelDetector(options);
+        // [END get_detector_cloud]
+        // [START run_detector_cloud]
+        Task<List<FirebaseVisionCloudLabel>> result =
+                detector.detectInImage(image)
+                        .addOnSuccessListener(
+                                new OnSuccessListener<List<FirebaseVisionCloudLabel>>() {
+                                    @Override
+                                    public void onSuccess(List<FirebaseVisionCloudLabel> labels) {
+                                        // Task completed successfully
+                                        // [START_EXCLUDE]
+                                        // [START get_labels_cloud]
+                                        textPrediction.setText("");
+                                        for (FirebaseVisionCloudLabel label : labels) {
+                                            String text = label.getLabel();
+                                            String entityId = label.getEntityId();
+                                            float confidence = label.getConfidence();
+                                            textPrediction.append(text + " " + String.format("%.3f", confidence) + "\n");
+                                            animalName = labels.get(0).getLabel();
+                                        }
+                                        // [END get_labels_cloud]
+                                        // [END_EXCLUDE]
+                                    }
+                                })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        textPrediction.setText("An unsuccessful attempt to connect to the server. Check your Internet connection.");
+                                    }
+                                });
+        // [END run_detector_cloud]
+    }
+}
---
+++ app/src/main/java/com/example/perkoz/animalfinder/Wikipedia.java
@@ -0,0 +1,79 @@
+package com.example.perkoz.animalfinder;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class Wikipedia extends AppCompatActivity {
+
+    private String animalName; // Animal name
+    private TextView textViewDescription; // Animal description
+    private ImageView imageDisplay; // Displayed image
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wikipedia);
+
+        Intent intent = getIntent();
+        Bitmap imageWikipedia = MainActivity.bitmapImage;
+
+        // Receive animal name from MainActivity
+        animalName = intent.getExtras().getString("animalname");
+        String url = "https://en.wikipedia.org/api/rest_v1/page/summary/" + animalName;
+
+        textViewDescription = (TextView) findViewById(R.id.textViewWikipedia);
+        textViewDescription.setMovementMethod(new ScrollingMovementMethod());
+        volleyJsonObjectRequest(url);
+
+        imageDisplay = (ImageView) findViewById(R.id.imageViewWikipedia);
+        imageDisplay.setImageBitmap(imageWikipedia);
+    }
+
+    // -------------------- wiki --------------------
+    public void volleyJsonObjectRequest(String url) {
+
+        String REQUEST_TAG = " com.androidtutorialpoint.volleyJsonObjectRequest";
+
+        JsonObjectRequest jsonObjectReq = new JsonObjectRequest(url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //response.toString());
+                        String text = null;
+                        try {
+                            text = response.getString("extract");
+                            textViewDescription.setText(text);
+                            textViewDescription.append("\n\nPowered by wikipedia.org");
+                        } catch (JSONException e) {
+                            textViewDescription.setText("Sorry. No information found about " + animalName +" on wikipedia.org");
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d("Error: " + error.getMessage());
+                textViewDescription.setText("Sorry. No information found about " + animalName +" on wikipedia.org");
+            }
+        });
+
+        // Adding JsonObject request to request queue
+        AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonObjectReq, REQUEST_TAG);
+
+    }
+}
