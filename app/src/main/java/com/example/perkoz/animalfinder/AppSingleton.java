9a1124da8c859932e43e6ca20bcbc4d34b785887
alan-bialy <37514209+alan-bialy@users.noreply.github.com>
2018-12-16 01:17:33 +0100

    Update AppSingleton.java

--- app/src/main/java/com/example/perkoz/animalfinder/AppSingleton.java
+++ app/src/main/java/com/example/perkoz/animalfinder/AppSingleton.java
@@ -9,9 +9,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
 
-/**
- * Created by androidtutorialpoint on 5/11/16.
- */
+
 public class AppSingleton {
     private static AppSingleton mAppSingletonInstance;
     private RequestQueue mRequestQueue;
